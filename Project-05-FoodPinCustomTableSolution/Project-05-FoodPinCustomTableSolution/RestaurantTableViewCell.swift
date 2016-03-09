53561cc84d7402621370463565da3021d0d103c5
beastlovezxc <beastkillerbin@gmail.com>
2016-03-09 22:47:51 +0800

    Project-05-FoodPinCustomTableSolution/

---
+++ Project-05-FoodPinCustomTableSolution/Project-05-FoodPinCustomTableSolution/RestaurantTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantTableViewCell.swift
+//  Project-05-FoodPinCustomTableSolution
+//
+//  Created by Bean on 16/3/9.
+//  Copyright © 2016年 Bean. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+}
