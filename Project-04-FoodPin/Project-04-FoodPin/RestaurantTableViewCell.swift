5b809b270c8508a2821b4736d79beb569361720a
beastlovezxc <beastkillerbin@gmail.com>
2016-03-09 20:17:26 +0800

    Project-04-FoodPin

---
+++ Project-04-FoodPin/Project-04-FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantTableViewCell.swift
+//  Project-04-FoodPin
+//
+//  Created by Bean on 16/3/9.
+//  Copyright © 2016年 Bean. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+}
---
+++ Project-04-FoodPin/README.md
@@ -0,0 +1,7 @@
+## FoodPin 实现
+
+一款饭店信息展示的TableController实现
+
+* 图片圆形展示
+* UITableViewCell 实现
+* Layer类应用  (cornerRadius, clipsToBounds)
