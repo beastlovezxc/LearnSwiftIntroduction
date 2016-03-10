335e69fa7722d5fe9ed1c9cde292fa4776b10df8
beastlovezxc <beastkillerbin@gmail.com>
2016-03-10 09:44:16 +0800

    update alertaction

--- Project-04-FoodPin/Project-04-FoodPin/RestaurantTableViewController.swift
+++ Project-04-FoodPin/Project-04-FoodPin/RestaurantTableViewController.swift
@@ -15,7 +15,7 @@ class RestaurantTableViewController: UITableViewController {
     var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
     
     var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
-    
+    var restaurantIsVisited = [Bool](count: 21, repeatedValue: false)
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! RestaurantTableViewCell
@@ -26,6 +26,12 @@ class RestaurantTableViewController: UITableViewController {
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
         cell.thumbnailImageView.layer.cornerRadius = 30.0
         cell.thumbnailImageView.clipsToBounds = true
+        
+        if restaurantIsVisited[indexPath.row] {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
         return cell
     }
     
@@ -39,5 +45,39 @@ class RestaurantTableViewController: UITableViewController {
     
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .ActionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        let callActionHandler = { (action: UIAlertAction!) -> Void in let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not avaliable yet. Please retry later", preferredStyle: .Alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alertMessage, animated: true, completion: nil)
+        }
+        let callAction = UIAlertAction(title: "Call" + "123-000-\(indexPath.row)", style: UIAlertActionStyle.Default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        if restaurantIsVisited[indexPath.row] {
+            let isVisitedAction = UIAlertAction(title: "I've not bean here", style: .Default, handler: {
+                (action: UIAlertAction) -> Void in
+                let cell = tableView.cellForRowAtIndexPath(indexPath)
+                cell?.accessoryType = .None
+                self.restaurantIsVisited[indexPath.row] = false
+            })
+            optionMenu.addAction(isVisitedAction)
+        } else {
+            let isVisitedAction = UIAlertAction(title: "I've bean here", style: .Default, handler: {
+                (action: UIAlertAction!) -> Void in
+                let cell = tableView.cellForRowAtIndexPath(indexPath)
+                cell?.accessoryType = .Checkmark
+                self.restaurantIsVisited[indexPath.row] = true
+            })
+            optionMenu.addAction(isVisitedAction)
+        }
+        
+                self.presentViewController(optionMenu, animated: true, completion: nil)
+        
+        //取消选中表格背景色
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
     }
 }
--- Project-04-FoodPin/README.md
+++ Project-04-FoodPin/README.md
@@ -5,3 +5,7 @@
 * 图片圆形展示
 * UITableViewCell 实现
 * Layer类应用  (cornerRadius, clipsToBounds)
+
+## 3.10 更新
+
+增加了弹出栏，右侧附加状态标记
